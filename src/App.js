8b9b77b8d8a6ec5a82e8f99211ac7f91a799389c
shehanp12 <shehanp068@gmail.com>
2020-10-30 02:17:41 +0530

    Balance.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import React from "react";
 import Header from "./components/Header";
+import {Balance} from "./components/Balance";
 
 function App() {
   return (
     <div className="App">
         <Header/>
+        <Balance/>
     </div>
   );
 }
