a499c3ad54ed2b590ce15ab90a3be6e760b9a5f8
shehanp12 <shehanp068@gmail.com>
2020-10-30 02:22:27 +0530

    IncomeExpenses.js

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './App.css';
 import React from "react";
 import Header from "./components/Header";
 import {Balance} from "./components/Balance";
+import {IncomeExpenses} from "./components/IncomeExpenses";
 
 function App() {
   return (
     <div className="App">
         <Header/>
         <Balance/>
+        <IncomeExpenses/>
     </div>
   );
 }
